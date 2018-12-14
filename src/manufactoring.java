db1e7b8643e3f6312f7ca0e23e1bd227d6d0beaa
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-12-14 14:15:11 -0500

    added lemon cup and sugar

--- src/manufactoring.java
+++ src/manufactoring.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class manufactoring {
     public static void main(String[] args){
-        
+
+double sugarPrice= 0.05;
+double lemonPrice=0.20;
+double cupPrice=0.20;
+double money=20.00;
+
+double sugar=0;
+double lemon=0;
+double cup=0;
+double day=7;
+
+System.out.println("Welcome to the lemonade stand.");
+while(day <= 7);
+
+System.out.println("Day" + day +"Money is at" + money);
+System.out.println("Your current inventory is: Lemons"+ lemon +", sugar:" + sugar+ "Cups are at:" +cup+",");
+System.out.println("***********************************\n");
+
+System.out.println("lemon cost" + lemonPrice+ "per unit: How many would you like to buy?");
+
+Scanner in1 = new Scanner(System.in);
+double boughtLemon= in1.nextDouble();
+if (money - (lemonPrice*boughtLemon)>=0)
+{
+    lemon+= lemon+ boughtLemon;
+
+    money= money - (lemonPrice*boughtLemon);
+
+}
+else if ((money - (lemonPrice*boughtLemon)<=0))
+{
+
+        }
+
+
+
+
     }
 }
